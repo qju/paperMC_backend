7f19bdf1a4fa893b41d96026b4958a587f548491
Qju <qjuman@gmail.com>
2026-05-30 17:51:49 +0200

    Update to the PaperMC download API.

--- internal/updater/paper_api.go
+++ internal/updater/paper_api.go
@@ -11,47 +11,48 @@ import (
 )
 
 // Endpoint: https://api.papermc.io/v2/projects/paper/versions/<version>/builds
-
+//
+// -- Ver3 Endpoint ---
+// https://fill.papermc.io/v3/projects/${PROJECT}/versions/${VERSION}/builds
+//
 // BuildsResponse represents the top-level JSON object
-type BuildsResponse struct {
-	ProjectId   string  `json:"project_id"`
-	ProjectName string  `json:"project_name"`
-	Version     string  `json:"version"`
-	Builds      []Build `json:"builds"`
+
+// -- New Endpoint API schema V3 ---
+type BuildV3 struct {
+	ID        int                     `json:"id"`
+	Time      time.Time               `json:"time"`
+	Channel   string                  `json:"channel"`
+	Commits   []Commit                `json:"commits"`
+	Downloads map[string]DownloadItem `json:"downloads"`
+}
+
+type Commit struct {
+	SHA     string    `json:"sha"`
+	Time    time.Time `json:"time"`
+	Message string    `json:"message"`
+}
+
+type DownloadItem struct {
+	Name      string    `json:"name"`
+	Checksums Checksums `json:"checksums"`
+	Size      int64     `json:"size"`
+	URL       string    `json:"url"`
+}
+
+type Checksums struct {
+	SHA256 string `json:"sha256"`
 }
 
 // Build represents a single entry in the "builds" list
-type Build struct {
-	Build     int       `json:"build"`
-	Time      string    `json:"time"`
-	Channel   string    `json:"channel"`
-	Promoted  bool      `json:"promoted"`
-	Changes   []Change  `json:"changes"`
-	Downloads Downloads `json:"downloads"`
-}
-
-type Change struct {
-	Commit  string `json:"commit"`
-	Summary string `json:"summary"`
-	Message string `json:"message"`
-}
-
-type Downloads struct {
-	Application Application `json:"application"`
-}
-
-type Application struct {
-	Name   string `json:"name"`
-	Sha256 string `json:"sha256"`
-}
 
 func GetLatestBuild(version string) (int, string, string, error) {
 	client := &http.Client{Timeout: 10 * time.Second}
-	url := fmt.Sprintf("https://api.papermc.io/v2/projects/paper/versions/%s/builds", version)
+	//url := fmt.Sprintf("https://fill.papermc.io/v3/projects/paper/versions/%s/builds/latest", version)
+	url := fmt.Sprintf("https://fill.papermc.io/v3/projects/paper/versions/%s/builds/latest", version)
 
 	resp, err := client.Get(url)
 	if err != nil {
-		return 0, "", "", fmt.Errorf("failed to fetch builds: %w", err)
+		return 0, "", "", fmt.Errorf("failed to fetch build: %w", err)
 	}
 	defer resp.Body.Close()
 
@@ -59,20 +60,15 @@ func GetLatestBuild(version string) (int, string, string, error) {
 		return 0, "", "", fmt.Errorf("api error: %d", resp.StatusCode)
 	}
 
-	var result BuildsResponse
+	var result BuildV3
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return 0, "", "", fmt.Errorf("invalid JSON: %w", err)
 	}
 
-	if len(result.Builds) == 0 {
-		return 0, "", "", fmt.Errorf("no builds found for version: %s", version)
-	}
-
 	// The API returns build sorted by time, so the last one is the latest
-	latest := result.Builds[len(result.Builds)-1]
-	return latest.Build,
-		latest.Downloads.Application.Name,
-		latest.Downloads.Application.Sha256,
+	return result.ID,
+		result.Downloads["server:default"].URL,
+		result.Downloads["server:default"].Name,
 		nil
 }
 
